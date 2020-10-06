27061c844b672e9cfec10d4b756cbba123219cab
brownb202 <brownb202@gmail.com>
2020-10-05 18:54:32 -0600

    chenged server to work with parent dir

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 9001;
 // const getApiData = require('./api-helper.js');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(`${__dirname}/../client/dist`));
+app.use(express.static('../client/dist'));
 
 // Requests here
 
